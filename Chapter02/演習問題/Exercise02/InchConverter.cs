7dba65ef4deef9a8aac9d988b4ad35e20a36e62b
KanekoShuya <oibc36271@ojs.ac.jp>
2025-04-22 11:47:58 +0900

    演習問題2.2を作成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class InchConverter {
+        //インチからメートルを求める
+        public static double InchToMeter(double inch) {
+            return inch * 0.0254;
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        //1～10インチをメートルに変換
+        static void Main(string[] args) {
+            InchToMeterList(1, 10);
+
+            //インチからメートルの対応表を出力
+            static void InchToMeterList(int start, int end) {
+                for (int inch = start; inch <= end; inch++) {
+                    double meter = InchConverter.InchToMeter(inch);
+                    Console.WriteLine($"{inch}in = {meter:0.0000}m");
+                }
+            }
+        }
+    }
+}
