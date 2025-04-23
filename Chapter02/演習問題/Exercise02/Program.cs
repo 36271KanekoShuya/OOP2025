623650941a8dbbbd047addabb50c61e9b6b3b7a9
KanekoShuya <oibc36271@ojs.ac.jp>
2025-04-23 09:54:43 +0900

    機能追加①完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Exercise02 {
     internal class Program {
-        //インチをメートルに変換
+        //インチとメートルを相互変換
         static void Main(string[] args) {
             Console.Write("インチからメートルなら0,逆なら1を入力:");
             int judge = int.Parse(Console.ReadLine());
@@ -20,6 +20,12 @@
 
             //インチからメートルの対応表を出力
             static void InchToMeterList(int start, int end) {
+                if (start > end) {//降順での出力用
+                    for (int inch = start; inch >= end; inch--) {
+                        double meter = InchConverter.InchToMeter(inch);
+                        Console.WriteLine($"{inch}in = {meter:0.0000}m");
+                    }
+                }
                 for (int inch = start; inch <= end; inch++) {
                     double meter = InchConverter.InchToMeter(inch);
                     Console.WriteLine($"{inch}in = {meter:0.0000}m");
@@ -27,6 +33,12 @@
             }
             //メートルからインチの対応表を出力
             static void MeterToInchList(int start, int end) {
+                if (start > end) {//降順での出力用
+                    for (int meter = end; meter >= start; meter++) {
+                        double inch = InchConverter.MeterToInch(meter);
+                        Console.WriteLine($"{meter}m = {inch:0.0000}in");
+                    }
+                }
                 for (int meter = start; meter <= end; meter++) {
                     double inch = InchConverter.MeterToInch(meter);
                     Console.WriteLine($"{meter}m = {inch:0.0000}in");
