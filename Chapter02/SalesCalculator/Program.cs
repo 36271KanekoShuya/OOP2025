eabfc08ce236900437492006f94989d743533818
KanekoShuya <oibc36271@ojs.ac.jp>
2025-05-09 14:59:15 +0900

    メソッド移動

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(ReadSales(@"data\sales.csv"));
+            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales(@"data\sales.csv"));
             Dictionary<string,int> amountsPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<String,int> obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key}{obj.Value}");
@@ -9,25 +9,7 @@
 
         }
 
-        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {
-            //売り上げデータを入れるリストオブジェクトを作成
-            List<Sale> sales = new List<Sale>();
-            //ファイルを一斉読み込み
-            string[] lines = File.ReadAllLines(filePath);
-            foreach (string line in lines) {
-                string[] items = line.Split(',');
-                Sale sale = new Sale() {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2]),
-                };
-                sales.Add(sale);
-
-            }
-
-            return sales;
-        }
+       
 
 
 
--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -24,5 +24,24 @@ namespace SalesCalculator {
             }
             return dict;
         }
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            //売り上げデータを入れるリストオブジェクトを作成
+            List<Sale> sales = new List<Sale>();
+            //ファイルを一斉読み込み
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale() {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
+
+            }
+
+            return sales;
+        }
     }
 }
