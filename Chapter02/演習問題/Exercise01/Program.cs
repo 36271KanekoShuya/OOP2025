ef7007c94dab921940a1e900af5706b384c221e7
KanekoShuya <oibc36271@ojs.ac.jp>
2025-05-13 11:44:44 +0900

    出力形式等変更

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,6 +2,8 @@
     internal class Program {
         static void Main(string[] args) {
             //2.1.3
+            //歌の情報のインスタンスを生成し、格納
+
             var songs = new Song[] {
                 new Song("Let it be", "The Beatles", 243),
                 new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
@@ -9,14 +11,19 @@
                 new Song("Honesty", "Billy Joel", 231),
                 new Song("I Will Always Love You", "Whitney Houston", 273),
             };
-            printSongs(songs);
+            PrintSongs(songs);
         }
 
 
         //2.1.4
-        private static void printSongs(Song[] songs) {
+        /// <summary>
+        /// 歌の情報を出力するメソッド、演奏時間は分:秒に換算される
+        /// </summary>
+        /// <param name="songs"></param>
+        private static void PrintSongs(Song[] songs) {
             foreach (var song in songs) {
-                Console.WriteLine($"{song.Title} / {song.ArtistName} / {TimeSpan.FromSeconds(song.Length).ToString(@"mm\:ss")}");
+                Console.WriteLine($"Title＞{song.Title,-30} | Artist＞{song.ArtistName,-20}" +
+                                        $" | Length＞{TimeSpan.FromSeconds(song.Length):mm\\:ss}");
 
             }
         }
--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -7,11 +7,17 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     //2.1.1
     public class Song {
-        public string Title { get; set; } = string.Empty;
-        public string ArtistName { get; set; } = string.Empty;
-        public int Length { get; set; }
+        public string Title { get; private set; } = string.Empty;
+        public string ArtistName { get; private set; } = string.Empty;
+        public int Length { get; private set; }
 
         //2.1.2
+        /// <summary>
+        /// 歌の情報を格納するコンストラクタ
+        /// </summary>
+        /// <param name="title">歌のタイトル</param>
+        /// <param name="artistName">アーティスト名</param>
+        /// <param name="length">演奏時間</param>
         public Song(string title, string artistName, int length) {
             Title = title;
             ArtistName = artistName;
