7dddfbee03278889b78b465c09f088157f83b0aa
KanekoShuya <oibc36271@ojs.ac.jp>
2025-05-13 10:25:33 +0900

    2.1.1完成

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //2.1.3
+
+        }
+
+
+        //2.1.4
+        private static void printSongs(Song[] songs) {
+
+        }
+    }
+}
