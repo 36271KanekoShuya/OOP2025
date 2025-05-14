17c33cc371108413864941342852e3e272b7c8dc
KanekoShuya <oibc36271@ojs.ac.jp>
2025-05-14 10:18:49 +0900

    登録形式に変更

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,16 +2,30 @@
     internal class Program {
         static void Main(string[] args) {
             //2.1.3
-            //歌の情報のインスタンスを生成し、格納
-
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
-            PrintSongs(songs);
+            var regisongs = new List<Song>();
+            Console.WriteLine("***** 曲の登録 *****");
+            while (true) {
+                Console.Write("曲名＞");
+                string title = Console.ReadLine();
+                if (title.Equals("end")) {
+                    break;
+                }
+                Console.Write("アーティスト＞");
+                string artistName = Console.ReadLine();
+                Console.Write("演奏時間(秒)＞");
+                int length = int.Parse(Console.ReadLine());
+                var song = new Song(title, artistName, length);
+                regisongs.Add(song);
+            }
+            PrintSongs(regisongs);
+            //var songs = new Song[] {
+            //    new Song("Let it be", "The Beatles", 243),
+            //    new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+            //    new Song("Close To You", "Carpenters", 276),
+            //    new Song("Honesty", "Billy Joel", 231),
+            //    new Song("I Will Always Love You", "Whitney Houston", 273),
+            //};
+            
         }
 
 
@@ -20,7 +34,8 @@
         /// 歌の情報を出力するメソッド、演奏時間は分:秒に換算される
         /// </summary>
         /// <param name="songs"></param>
-        private static void PrintSongs(Song[] songs) {
+        private static void PrintSongs(List<Song> songs) {
+            Console.WriteLine("***** 登録曲一覧 *****");
             foreach (var song in songs) {
                 Console.WriteLine($"Title＞{song.Title,-30} | Artist＞{song.ArtistName,-20}" +
                                         $" | Length＞{TimeSpan.FromSeconds(song.Length):mm\\:ss}");
