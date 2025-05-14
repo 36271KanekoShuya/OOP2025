7a815ef9d058f1efdee064bebad87a9f9a63ef07
KanekoShuya <oibc36271@ojs.ac.jp>
2025-05-14 11:02:19 +0900

    0514演習完成、クラス分け

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,31 +2,19 @@
     internal class Program {
         static void Main(string[] args) {
             //2.1.3
-            var regisongs = new List<Song>();
             Console.WriteLine("***** 曲の登録 *****");
-            while (true) {
-                Console.Write("曲名＞");
-                string title = Console.ReadLine();
-                if (title.Equals("end")) {
-                    break;
-                }
-                Console.Write("アーティスト＞");
-                string artistName = Console.ReadLine();
-                Console.Write("演奏時間(秒)＞");
-                int length = int.Parse(Console.ReadLine());
-                var song = new Song(title, artistName, length);
-                regisongs.Add(song);
-            }
+            var regisongs = SongRegister.Register();
             PrintSongs(regisongs);
-            //var songs = new Song[] {
-            //    new Song("Let it be", "The Beatles", 243),
-            //    new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-            //    new Song("Close To You", "Carpenters", 276),
-            //    new Song("Honesty", "Billy Joel", 231),
-            //    new Song("I Will Always Love You", "Whitney Houston", 273),
-            //};
+            
             
         }
+        //var songs = new Song[] {
+        //    new Song("Let it be", "The Beatles", 243),
+        //    new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+        //    new Song("Close To You", "Carpenters", 276),
+        //    new Song("Honesty", "Billy Joel", 231),
+        //    new Song("I Will Always Love You", "Whitney Houston", 273),
+        //};
 
 
         //2.1.4
