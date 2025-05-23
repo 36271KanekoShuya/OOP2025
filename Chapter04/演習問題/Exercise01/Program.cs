a38c6c46b13423954a9eec4e2022c347b1056397
KanekoShuya <oibc36271@ojs.ac.jp>
2025-05-23 15:31:24 +0900

    4.2.1

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -43,7 +43,7 @@ namespace Exercise01 {
         }
 
         private static void Exercise3(List<string> langs) {
-            var lang = (langs.Find(s => s.Length == 10)) ?? "unknown";
+            var lang = langs.Find(s => s.Length == 10) ?? "unknown";
             Console.WriteLine(lang);
         }
     }
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1();
+            Console.WriteLine("---");
+            Exercise2();
+            Console.WriteLine("---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            var value = Console.ReadLine();
+            if (int.TryParse(value, out var num)) {
+                if (num < 0) {
+                    Console.WriteLine(num);
+                } else if (num < 100) {
+                    Console.WriteLine(num*2);
+                } else if (num < 500) {
+                    Console.WriteLine(num*3);
+                } else{
+                    Console.WriteLine(num);
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
+        }
+
+        private static void Exercise2() {
+
+        }
+
+        private static void Exercise3() {
+
+        }
+    }
+}
