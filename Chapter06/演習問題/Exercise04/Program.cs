73db460c198b9b68f568eea9223b4ece2d2493eb
KanekoShuya <oibc36271@ojs.ac.jp>
2025-06-03 10:41:23 +0900

    6,4完了

--- Chapter06/演習問題/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -2,10 +2,10 @@
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-
-
-
-
+            foreach (var key in line.Split(';')) {
+                var quart = key.Split('=');
+                Console.WriteLine($"{ToJapanese(quart[0])}:{quart[1]}");
+            }
         }
 
         /// <summary>
@@ -14,7 +14,23 @@
         /// <param name="key">"Novelist","BestWork","Born"</param>
         /// <returns>"「作家」,「代表作」,「誕生年」</returns>
         static string ToJapanese(string key) {
+            return key switch {
+                "Novelist" => "作家",
+                "BestWork" => "代表作",
+                "Born" => "誕生年",
+                _ => "error"
+            };
 
+            //switch (key) {
+            //    case "Novelist":
+            //        return "作家";
+            //    case "BestWork":
+            //        return "代表作";
+            //    case "Born":
+            //        return "誕生年";
+            //    default:
+            //        return "error";
+            //}
             return ""; //エラーをなくすためのダミー
         }
     }
