d120cd0d500952621c69a9e08f4895c12450175a
KanekoShuya <oibc36271@ojs.ac.jp>
2025-06-10 09:57:52 +0900

    5.2完了、5.1.3微修正

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -15,7 +15,6 @@ namespace Exercise01 {
             this.month = month;
         }
 
-
         //5.1.2
         //設定されている西暦が21世紀か判定する
         //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
@@ -24,13 +23,14 @@ namespace Exercise01 {
         //5.1.3
         public YearMonth AddOneMonth() {
             YearMonth rtn;
-            if (year == 12) {
+            if (year < 11) {
                 rtn = new YearMonth(year + 1, 1);
             } else {
                 rtn = new YearMonth(year, month + 1);
             }
             return rtn;
         }
+
         //5.1.4
         public override string ToString() => $"{year}年{month}月";
     }
--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -1,7 +1,54 @@
-﻿namespace Exercise02 {
+﻿using Exercise01;
+
+namespace Exercise02 {
     public class Program {
         static void Main(string[] args) {
-            
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
+
+        private static void Exercise2(YearMonth[] ymCollection) {
+            for (int i = 0; i < ymCollection.Length; i++) {
+                Console.WriteLine(ymCollection[i]);
+            }
+        }
+
+        public static YearMonth? Rtn21Cen(YearMonth[] ymCollection) {
+            foreach (var years in ymCollection) {
+                if (years.Is21Century) {
+                    return years;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise4(YearMonth[] ymCollection) {
+            var find21 = Rtn21Cen(ymCollection);
+
+            if (find21 is null) {
+                Console.WriteLine("21世紀のデータはありません");
+            } else {
+                Console.WriteLine(find21);
+            }
+        }
+
+        private static void Exercise5(YearMonth[] ymCollection) {
+            ymCollection.ToList().ForEach(x=> Console.WriteLine(x.AddOneMonth()));
         }
     }
 }
