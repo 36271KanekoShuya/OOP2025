ad44c2265c9b73f3dd1826afd13f171a0481c900
KanekoShuya <oibc36271@ojs.ac.jp>
2025-06-17 10:19:52 +0900

    8.2.3完了

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -3,38 +3,26 @@
         static void Main(string[] args) {
             // コンストラクタの呼び出し
             var abbrs = new Abbreviations();
-
             // Addメソッドの呼び出し例
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
 
-            // Getメソッドの利用例
-            var names = new[] { "WHO", "FIFA", "NPT", };
-            foreach (var name in names) {
-                var fullname = abbrs.Get(name);
-                if (fullname is null) {
-                    Console.WriteLine($"{name}は見つかりません");
-                } else {
-                    Console.WriteLine($"{name}={fullname}");
-                }
-            }
+            //8.2.3(Countの呼び出し例)
+            Console.WriteLine($"用語数:{abbrs.Count}");
             Console.WriteLine();
 
-            // ToAbbreviationメソッドの利用例
-            var japanese = "東南アジア諸国連合";
-            var abbreviation = abbrs.ToAbbreviation(japanese);
-            if (abbreviation is null) {
-                Console.WriteLine($"{japanese} は見つかりません");
-            } else {
-                Console.WriteLine($"「{japanese}」の略語は {abbreviation} です");
+            //8.2.3(Removeの呼び出し例)
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine("NPTを削除します");
+                Console.WriteLine($"単語数:{abbrs.Count}");
             }
             Console.WriteLine();
-
-            // FindAllメソッドの利用例
-            foreach (var (key, value) in abbrs.FindAll("国際")) {
-                Console.WriteLine($"{key}={value}");
+            //すでに消したものへRemove
+            if (!abbrs.Remove("NPT")) {
+                Console.WriteLine("存在しない用語です");
             }
-            Console.WriteLine();
+            //8.2.4(3文字の省略後だけ全て出力)
+            
         }
     }
 }
