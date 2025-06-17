0d0203050dcdcfee37852e4d9d3f46e29096b90e
KanekoShuya <oibc36271@ojs.ac.jp>
2025-06-17 11:18:39 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; private set; } = string.Empty;//学生の名前
+        public string Subject { get; private set; } = string.Empty;//科目名
+        public int Score { get; private set; }//点数
+
+    }
+}
