a1d70046ddaa11defa0602063f5be7c7828c88cd
KanekoShuya <oibc36271@ojs.ac.jp>
2025-06-24 14:25:04 +0900

    生まれてから何日目か 完了

--- Chapter09/Section01/Program.cs
+++ Chapter09/Section01/Program.cs
@@ -4,11 +4,11 @@ namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
 
-            //var today = DateTime.Today;//日付
-            var todays = new DateTime(2025, 7, 12);
+            var today = DateTime.Today;//日付
+            //var todays = new DateTime(2025, 7, 12);
             var now = DateTime.Now;//日付と時刻
 
-            Console.WriteLine($"Today:{todays}");
+            Console.WriteLine($"Today:{today}");
             Console.WriteLine($"Now:{now}");
             //打ち込んだ年月日は何曜日か調べる
             Console.Write("年:");
@@ -24,13 +24,19 @@ namespace Section01 {
             var str = ymd.ToString("ggyy年M月d日",culture);
             var dayname = culture.DateTimeFormat.GetDayName(ymd.DayOfWeek);
             Console.WriteLine($"{str}は{dayname}です");
-            
+            //その日から現在まで何日目か
+            TimeSpan tonow = DateTime.Today - ymd;
+            Console.WriteLine($"その日から{tonow.Days}日目です");
+            //年月日を閏年か判定する
             var leapyear = DateTime.IsLeapYear(year);
             if (leapyear) {
                 Console.WriteLine("閏年です");
             } else {
                 Console.WriteLine("閏年ではありません");
             }
+
+
+
         }
     }
 }
