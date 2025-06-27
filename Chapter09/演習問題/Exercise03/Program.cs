f6d8b9df8b7bea240d7897171138358659704a39
KanekoShuya <oibc36271@ojs.ac.jp>
2025-06-27 11:23:09 +0900

    9.2完了

---
+++ Chapter09/演習問題/Exercise03/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var tw = new TimeWatch();
+            tw.Start();
+            // スリープする
+            Thread.Sleep(1000);
+            TimeSpan duration = tw.Stop();
+            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+
+        }
+    }
+
+    class TimeWatch {
+        private DateTime _time;
+        
+        public void Start() {
+            //現在の時間を_timeに設定
+            
+        }
+
+        public TimeSpan Stop() {
+            //経過時間を返却する
+
+            return new TimeSpan(); // ←エラーを出さないためだけのダミー（使い方も参考にしない）
+        }
+    }
+}
