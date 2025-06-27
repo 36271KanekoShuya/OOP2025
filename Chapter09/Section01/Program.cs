1202c256e83b69e555439039e6f85d571e65cea3
KanekoShuya <oibc36271@ojs.ac.jp>
2025-06-27 10:06:03 +0900

    9.1完了

--- Chapter09/Section01/Program.cs
+++ Chapter09/Section01/Program.cs
@@ -10,6 +10,7 @@ namespace Section01 {
 
             Console.WriteLine($"Today:{today}");
             Console.WriteLine($"Now:{now}");
+
             //打ち込んだ年月日は何曜日か調べる
             Console.Write("年:");
             var year = int.Parse(Console.ReadLine());
@@ -21,12 +22,21 @@ namespace Section01 {
             DateTime ymd = new DateTime(year, month, day);
             var culture = new CultureInfo("ja-JP");
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
-            var str = ymd.ToString("ggyy年M月d日",culture);
+            var str = ymd.ToString("ggyy年M月d日", culture);
             var dayname = culture.DateTimeFormat.GetDayName(ymd.DayOfWeek);
             Console.WriteLine($"{str}は{dayname}です");
+
             //その日から現在まで何日目か
             TimeSpan tonow = DateTime.Today - ymd;
-            Console.WriteLine($"その日から{tonow.Days}日目です");
+            Console.WriteLine($"その日から{tonow.Days + 1}日目です");
+
+            //その日を生年月日とした年齢
+            Console.WriteLine($"あなたは{tonow.Days / 365}歳です");
+
+            //1月1日から何日目か
+            TimeSpan nowdays = DateTime.Today - new DateTime(today.Year, 1, 1);
+            Console.WriteLine($"1月1日から{nowdays.Days + 1}日目です");
+
             //年月日を閏年か判定する
             var leapyear = DateTime.IsLeapYear(year);
             if (leapyear) {
@@ -35,8 +45,12 @@ namespace Section01 {
                 Console.WriteLine("閏年ではありません");
             }
 
-
-
+            // 現在時刻を取得
+            while (true) {
+                DateTime nowtime = DateTime.Now;
+                Console.WriteLine(now.ToString("yyyy/MM/dd HH:mm:ss"));
+                Thread.Sleep(100);
+            }
         }
     }
 }
---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            Console.WriteLine(string.Format("{0:yyyy/MM/dd hh:mm}",dateTime));
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            Console.WriteLine(dateTime.ToString("yyyy年MM月dd日 hh時mm分ss秒"));
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = dateTime.ToString("ggy年M月d日",culture);
+            var dayname = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            Console.WriteLine($"{str} ({dayname})");
+        }
+    }
+}
