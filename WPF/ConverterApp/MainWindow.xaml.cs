1aad5a16e8ff55e25d3aaea60e8e478c4f05c5ec
KanekoShuya <oibc36271@ojs.ac.jp>
2025-10-02 14:59:22 +0900

    ボタンでの文字列変更

--- WPF/ConverterApp/MainWindow.xaml.cs
+++ WPF/ConverterApp/MainWindow.xaml.cs
@@ -18,8 +18,30 @@ namespace ConverterApp {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        private Dictionary<string,int> Metricunits = new Dictionary<string,int>() {
+            {"mm",1 },
+            {"cm",10 },
+            {"m", 10*100 },
+            {"km", 10*100*1000},
+        };
+        private Dictionary<string, int> Imperialunits = new Dictionary<string, int>() {
+            {"in",1 },
+            {"ft",12 },
+            {"yd",12*3 },
+            {"ml",12*3*1760},
+        };
         public MainWindow() {
             InitializeComponent();
+            MetricUnit.ItemsSource = Metricunits;
+            ImperialUnit.ItemsSource = Imperialunits;
+        }
+
+        private void ImperialUnit_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+        public class DisUnits {
+            public string name { get; set; }
+            public double coefficient {  get; set; }
         }
     }
 }
---
+++ WPF/HelloWorld/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld {
+    class ViewModel : BindableBase {
+        public ViewModel() {
+            ChangeMessageCommand = new DelegateCommand(
+                () => GreetingMessage = "Bye-bye world");
+        }
+
+        private string _greetingMessage = "Hello World";
+        public string GreetingMessage {
+            get => _greetingMessage;
+            set => SetProperty(ref _greetingMessage, value);
+        }
+        public DelegateCommand ChangeMessageCommand { get; }
+    }
+}
