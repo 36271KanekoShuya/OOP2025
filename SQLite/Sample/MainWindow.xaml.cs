c12500092a970813427f1d89a62dbe60c7e605b3
KanekoShuya <oibc36271@ojs.ac.jp>
2025-10-15 12:07:33 +0900

    ObservableCollectionまで

---
+++ SQLite/Sample/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Sample.Data;
+using SQLite;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private ObservableCollection<Person> _persons = new ObservableCollection<Person>();
+        public MainWindow() {
+            InitializeComponent();
+            ReadDatabase();
+            //PersonListView.ItemsSource = _persons;
+        }
+
+        private void ReadDatabase() {
+            using (var connection = new SQLiteConnection(App.databasePath)) {
+                connection.CreateTable<Person>();
+                PersonListView.ItemsSource = connection.Table<Person>().ToList();
+                
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e) {
+            var person = new Person() {
+                Name = NameTextBox.Text,
+                Phone = PhoneTextBox.Text,
+            };
+
+            using (var connection = new SQLiteConnection(App.databasePath)) {
+                connection.CreateTable<Person>();
+                connection.Insert(person);
+            }
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e) {
+            ReadDatabase();
+        }
+    }
+}
