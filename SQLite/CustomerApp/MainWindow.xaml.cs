1aab2b02a22b8d8befdf5c49174f9e46fc4cebf1
KanekoShuya <oibc36271@ojs.ac.jp>
2025-10-22 10:51:09 +0900

    CusApp UI 一段落

---
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CustomerApp.Data;
+using SQLite;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomerApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private List<Customer> _persons = new List<Customer>();
+        public MainWindow() {
+            InitializeComponent();
+
+        }
+
+        private void ReadDatabase() {
+            using (var connection = new SQLiteConnection(App.databasePath)) {
+                connection.CreateTable<Customer>();
+                _persons = connection.Table<Customer>().ToList();
+
+            }
+            PersonListView.ItemsSource = _persons;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void PersonListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void PictureButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
