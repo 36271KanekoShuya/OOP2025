db4960cb405e2fa34b448a3898e48e2a9837e16b
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-05 11:17:14 +0900

    コード14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient();
+            await GetHtmlExample(hc);
+        }
+
+        static async Task GetHtmlExample(HttpClient http) {
+            var url = "https://abehiroshi.la.coocan.jp/";
+            var text = await http.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
