70a0b7dbfb77cb243e0793c33ae547421e6b8527
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-06 14:41:49 +0900

    WebView 完了

--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Http;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Net.Http;
 using System.Security.Policy;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,20 +21,41 @@ namespace WebBrowser {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-
+            InirializeAsync();
+        }
+        private async void InirializeAsync() {
+            await WebView.EnsureCoreWebView2Async();
+            WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+            WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+        }
+        //プログレスバー表示
+        private void CoreWebView2_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+            LoadingBar.Visibility = Visibility.Visible;
+            LoadingBar.IsIndeterminate =true;
+        }
+        //プログレスバー非表示
+        private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e) {
+            LoadingBar.Visibility = Visibility.Collapsed;
+            LoadingBar.IsIndeterminate = false;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e) {
-            WebView.GoBack();
+            if (WebView.CanGoBack) {
+                WebView.GoBack();
+            }
+
         }
 
         private void FowardButton_Click(object sender, RoutedEventArgs e) {
-            WebView.GoForward();
+            if (WebView.CanGoForward) {
+                WebView.GoForward();
+            }
+
         }
 
         private async void GoButton_Click(object sender, RoutedEventArgs e) {
             try {
-                WebView.Source = new Uri(AddressBar.Text);
+                WebView.Source = new Uri(AddressBar.Text.Trim());
 
             }
             catch (InvalidOperationException) {
@@ -40,7 +63,7 @@ namespace WebBrowser {
                 return;
             }
             catch (Exception ex) {
-                MessageBox.Show (ex.Message);
+                MessageBox.Show(ex.Message);
                 return;
             }
         }
