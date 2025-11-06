1b32d9804817954411c542ceeb9b63f7d165a980
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-06 15:17:30 +0900

    並列処理の対照実験

---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Parallel.For(0, 100, i => {
+                Console.WriteLine($"処理a-{i}開始");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理a-{i}終了");
+            });
+
+
+            Console.WriteLine("--------並列処理なし-------");
+            for(int i = 0; i < 10; i++) {
+                Console.WriteLine($"処理b-{i}開始");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理b-{i}終了");
+            }
+        }
+    }
+}
