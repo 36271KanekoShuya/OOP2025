162d26d3d6994c04f2be0354fff514a5dd34351a
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-06 15:57:22 +0900

    14_20テスト

--- Chapter14/Section05/Program.cs
+++ Chapter14/Section05/Program.cs
@@ -1,20 +1,62 @@
-﻿namespace Section05 {
+﻿using Section01;
+using System.Diagnostics;
+using System.Threading.Tasks;
+namespace Section05 {
     internal class Program {
-        static void Main(string[] args) {
+        static async Task Main(string[] args) {
+            await Code14_20.Run();
+        }
+    }
 
-            Parallel.For(0, 100, i => {
-                Console.WriteLine($"処理a-{i}開始");
-                Thread.Sleep(1000);
-                Console.WriteLine($"処理a-{i}終了");
-            });
+    public class Code14_20 {
 
+        public static async Task Run() {
+            var sw = Stopwatch.StartNew();
+            var task1 = Task.Run(() => Get5000thPrime());
+            var task2 = Task.Run(() => Get6000thPrime());
+            var prime1 = await task1;
+            var prime2 = await task2;
+            sw.Stop();
+            Console.WriteLine(prime1);
+            Console.WriteLine(prime2);
+            Console.WriteLine($"実行時間: {sw.ElapsedMilliseconds}ミリ秒");
+        }
 
-            Console.WriteLine("--------並列処理なし-------");
-            for(int i = 0; i < 10; i++) {
-                Console.WriteLine($"処理b-{i}開始");
-                Thread.Sleep(1000);
-                Console.WriteLine($"処理b-{i}終了");
+        public static void Run2() {
+            var sw = Stopwatch.StartNew();
+            var prime1 = Get5000thPrime();
+            var prime2 = Get6000thPrime();
+            sw.Stop();
+            Console.WriteLine(prime1);
+            Console.WriteLine(prime2);
+            Console.WriteLine($"実行時間: {sw.ElapsedMilliseconds}ミリ秒");
+        }
+        // 5000番目の素数を求める
+        private static int Get5000thPrime() {
+            return GetPrimes().Skip(4999).First();
+        }
+
+        // 6000番目の素数を求める
+        private static int Get6000thPrime() {
+            return GetPrimes().Skip(5999).First();
+        }
+
+        // 上記2つのメソッドから呼び出される下位メソッド
+        // あえて効率の悪いアルゴリズムで記述している
+        public static IEnumerable<int> GetPrimes() {
+            for (int i = 2; i < int.MaxValue; i++) {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                    yield return i;
             }
         }
     }
 }
+
+
