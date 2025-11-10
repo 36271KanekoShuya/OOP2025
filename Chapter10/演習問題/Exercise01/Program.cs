d6489b001603b4e2e43213acd76386604997f810
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-10 14:19:44 +0900

    演習10-1

---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var filePath = "source.txt";
+            CountClass1(filePath);
+            CountClass2(filePath);
+            CountClass3(filePath);
+        }
+
+        private static void CountClass3(string filePath) {
+            Console.WriteLine(File.ReadLines(filePath, Encoding.UTF8)
+                .Count(x => x.Contains("class")));
+        }
+
+        private static void CountClass2(string filePath) {
+            Console.WriteLine(File.ReadAllLines(filePath, Encoding.UTF8)
+                .Count(x => x.Contains("class")));
+        }
+
+        private static void CountClass1(string filePath) {
+            if (File.Exists(filePath)) {
+                int cnt = 0;
+                using var reader = new StreamReader(filePath, Encoding.UTF8);
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine().Split(' ')
+                        .ToList().Where(x => x.Contains("class"));
+                    if (line.Count() > 0) {
+                        cnt++;
+                    }
+                }
+                Console.WriteLine(cnt);
+            }
+        }
+    }
+}
