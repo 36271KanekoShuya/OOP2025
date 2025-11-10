601d5a32fe71e0b88f8b0e3dcca9ebf2589a0031
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-10 15:59:56 +0900

    空白除去

--- Chapter10/演習問題/Exercise03/Program.cs
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -2,13 +2,18 @@
     internal class Program {
         static void Main(string[] args) {
             Console.Write("書き足されるファイル＞");
-            var writeFile = Console.ReadLine() ?? "source.txt";
+            var writeFile = Console.ReadLine();
+            if (writeFile == "") {
+                writeFile = "source.txt";
+            }
             Console.Write("書き足すファイル＞");
-            var readFile = Console.ReadLine() ?? "source2.txt";
-            var text1 = File.ReadAllLines(writeFile);
-            var text2 = File.ReadAllLines(readFile);
+            var readFile = Console.ReadLine() ;
+            if (readFile == "") {
+                readFile = "source2.txt";
+            }
+            var text = File.ReadAllLines(readFile);
             using (var writer = new StreamWriter(writeFile, append: true)) {
-                foreach (var line in text2) {
+                foreach (var line in text) {
                     writer.WriteLine(line);
                 }
             }
