b170b5f76571d13554e2b132be16a42333d92cc8
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-12 12:11:30 +0900

    LineCounter 検索

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 using TextFileProcessor;
 
 namespace LineCounter {
-    internal class LineCounterProcessor : TextProcessor{
+    internal class LineCounterProcessor : TextProcessor {
         private int _count = 0;
+        string search = string.Empty;
+        protected override void Initialize(string fname) {
+            _count = 0;
+            Console.Write("検索する単語を入力＞");
+            search = Console.ReadLine();
+        }
 
-        protected override void Initialize(string fname) => _count = 0;
-
-        protected override void Execute(string line) => _count++;
+        protected override void Execute(string line) {
+            if (line.Contains(search)) {
+                _count++;
+            }
+        }
 
         protected override void Terminate() => Console.WriteLine("{0}行", _count);
     }
