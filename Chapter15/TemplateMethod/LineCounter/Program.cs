38393dd37c0dd9de53b72a4460a637939137fa98
KanekoShuya <oibc36271@ojs.ac.jp>
2025-11-12 11:31:59 +0900

    LineCounter

---
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -0,0 +1,8 @@
+﻿using TextFileProcessor;
+namespace LineCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            TextProcessor.Run<LineCounterProcessor>(args[0]);
+        }
+    }
+}
